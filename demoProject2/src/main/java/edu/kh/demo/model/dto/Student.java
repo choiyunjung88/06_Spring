f427ec6ac89de84621a79b68c978c65650b31a06
choiyunjung88 <lucy023@sookmyung.ac.kr>
2024-04-09 17:44:58 +0900

    Day53 04/09

--- demoProject2/src/main/java/edu/kh/demo/model/dto/Student.java
+++ demoProject2/src/main/java/edu/kh/demo/model/dto/Student.java
@@ -6,19 +6,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
-// Spring EL 같은 경우 getter 가 필수로 작성되어있어야 한다
+// Spring EL 같은 경우 getter가 필수로 작성되어있어야 한다
 // -> ${Student.getName()} == ${Student.name}
-//getter 대신필드면 호출하는 형식으로 작성하는데
-//자동으로 getter 호출하기 때문
+// getter 대신 필드명 호출하는 형식으로 작성하는데
+// 자동으로 getter 호출하기 때문
 
-@AllArgsConstructor // 모든 필드를 초기화 하는 용도의 매개변수 생성자
-@NoArgsConstructor // 매개변수 없는 생성자 (== 기본 생성자)
-@ToString
-@Setter
 @Getter
+@Setter
+@ToString
+@NoArgsConstructor // 매개변수 없는 생성자 (== 기본 생성자)
+@AllArgsConstructor // 모든 필드를 초기화하는 용도의 매개변수 생성자
 public class Student {
 	private String studentNo; // 학번
-	private String name;
-	private int age;
+	private String name; // 이름
+	private int age; // 나이
 }
