0b5ecc12b7eaa027608a3fe7fef1060154b596ac
choiyunjung88 <lucy023@sookmyung.ac.kr>
2024-04-18 17:31:35 +0900

    Day59 04/18

--- boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,7 +1,9 @@
 package edu.kh.project.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -11,5 +13,12 @@ public class MainController {
 		
 		// 접두사/접미사 제외
 		return "common/main";
+	}
+	
+	// LoginFilter
+	@GetMapping("loginError")
+	public String loginError(RedirectAttributes ra) {
+		ra.addFlashAttribute("message", "로그인 후 이용해 주세요~");
+		return "redirect:/";
 	}
 }
