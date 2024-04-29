c4abc27540359ac89d7f9c49a4973a379729ebad
choiyunjung88 <lucy023@sookmyung.ac.kr>
2024-04-29 17:53:11 +0900

    Day65 04/29

---
+++ boardProject-boot/src/main/java/edu/kh/project/board/model/service/CommentService.java
@@ -0,0 +1,17 @@
+package edu.kh.project.board.model.service;
+
+import java.util.List;
+
+import edu.kh.project.board.model.dto.Comment;
+
+public interface CommentService {
+
+	List<Comment> select(int boardNo);
+
+	int insert(Comment comment);
+
+	int update(Comment comment);
+
+	int delete(int comment);
+
+}
